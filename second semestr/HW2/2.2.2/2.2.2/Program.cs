cc70d10ba2de8fc4b4144ac959a1fb4dac81d155
Dasha-Belozerceva <d_belosertseva@mail.ru>
2015-03-21 17:13:57 +0300

    list!

---
+++ second semestr/HW2/2.2.2/2.2.2/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ListNamespace;
+
+namespace _2._2._2
+{
+    class Program
+    {
+        static void Main()
+        {
+            List list = new List();
+        }
+    }
+}
