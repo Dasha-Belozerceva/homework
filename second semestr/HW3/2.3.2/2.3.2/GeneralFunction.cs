a7973a34b983fc28b176fc865d80637d7edb3ad5
Dasha-Belozerceva <d_belosertseva@mail.ru>
2015-03-30 17:27:02 +0300

    added task 3.2

---
+++ second semestr/HW3/2.3.2/2.3.2/GeneralFunction.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2._3._2
+{
+    public interface GeneralFunction
+    {
+        /// <summary>
+        /// inteface to switch Hash Functions
+        /// </summary>
+        int HashFunction(string element);
+    }
+}
---
+++ second semestr/HW3/2.3.2/2.3.2/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using RealizationOfTheHashTable;
+using _2._3._2;
+
+namespace _2._3._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            GeneralFunction hashFunction = new hashFunction1();
+            Hash hash = new Hash(hashFunction);
+
+        }
+    }
+}
