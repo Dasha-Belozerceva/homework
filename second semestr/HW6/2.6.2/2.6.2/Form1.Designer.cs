a6e877240d28ab481994e78e1da474a741952194
Dasha-Belozerceva <d_belosertseva@mail.ru>
2015-05-26 21:30:30 +0300

    digital clock

---
+++ second semestr/HW6/2.6.2/2.6.2/Form1.Designer.cs
@@ -0,0 +1,64 @@
+﻿namespace _2._6._2
+{
+    partial class DigitalClock
+    {
+        /// <summary>
+        /// Требуется переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Обязательный метод для поддержки конструктора - не изменяйте
+        /// содержимое данного метода при помощи редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Time = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // Time
+            // 
+            this.Time.AutoSize = true;
+            this.Time.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.Time.Location = new System.Drawing.Point(12, 9);
+            this.Time.Name = "Time";
+            this.Time.Size = new System.Drawing.Size(284, 73);
+            this.Time.TabIndex = 0;
+            this.Time.Text = "00:00:00";
+            // 
+            // DigitalClock
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(306, 95);
+            this.Controls.Add(this.Time);
+            this.Name = "DigitalClock";
+            this.Text = "Digital Clock";
+            this.Load += new System.EventHandler(this.DigitalClock_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label Time;
+
+    }
+}
+
