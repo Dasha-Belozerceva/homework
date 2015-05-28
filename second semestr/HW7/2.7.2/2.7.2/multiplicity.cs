790d4d3bab33f3a25cbb438d110e82cc9e01bfaa
Dasha-Belozerceva <d_belosertseva@mail.ru>
2015-05-29 02:54:25 +0300

    multiplicity

---
+++ second semestr/HW7/2.7.2/2.7.2/multiplicity.cs
@@ -0,0 +1,197 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2._7._2
+{
+    /// <summary>
+    /// class of multiplicity
+    /// </summary>
+    public  class multiplicity<T>
+    {
+        /// <summary>
+        /// List element
+        /// </summary>
+        private class MultiplicityElement
+        {
+            /// <summary>
+            /// value of list element
+            /// </summary>
+            public T Value { get; set; }
+
+            /// <summary>
+            /// move point to the next element
+            /// </summary>
+            public MultiplicityElement Next { get; set; }
+        }
+
+        /// <summary>
+        /// the first list element
+        /// </summary>
+        private MultiplicityElement head = null;
+
+        /// <summary>
+        /// Chek of existence elements in List.
+        /// </summary>
+        /// <returns></returns>
+        public bool IsEmpty()
+        {
+            return head == null;
+        }
+
+        /// <summary>
+        /// insert element to multiplicity
+        /// </summary>
+        /// <param name="value"></param>
+        public void AddElement(T value)
+        {
+            var newElement = new MultiplicityElement()
+            {
+                Value = value,
+                Next = null,
+            };
+
+            if (IsEmpty())
+            {
+                head = newElement;
+                return;
+            }
+
+            MultiplicityElement currentElement = head;
+
+            while (currentElement.Next != null)
+            {
+                if (currentElement.Next == null)
+                {
+                    return;
+                }
+                if (currentElement.Value.Equals(value))
+                {
+                    return;
+                }
+                currentElement = currentElement.Next;
+            }
+            currentElement.Next = newElement;
+        }
+
+        /// <summary>
+        /// remove element from multiplicity
+        /// </summary>
+        /// <param name="value"></param>
+        public void DeleteElement(T value)
+        {
+            MultiplicityElement currentElement = head;
+            MultiplicityElement previousElement = head;
+
+            while (currentElement != null)
+            {
+                if (head.Value.Equals(value))
+                {
+                    head = head.Next;
+                    currentElement = null;
+                    return;
+                }
+                if (!currentElement.Value.Equals(value))
+                {
+                    previousElement = currentElement;
+                    currentElement = currentElement.Next;
+                }
+                else
+                {
+                    previousElement.Next = currentElement.Next;
+                    currentElement = null;
+                    return;
+                }
+            }
+        }
+
+
+        /// <summary>
+        /// check element for existence in multiplicity
+        /// </summary>
+        /// <param name="value"></param>
+        /// <returns></returns>
+        public bool availabilityOFValue(T value)
+        {
+            if (IsEmpty())
+            {
+                return false;
+            }
+
+            MultiplicityElement currentElement = head;
+
+            while (currentElement != null)
+            {
+                if (!currentElement.Value.Equals(value))
+                {
+                    currentElement = currentElement.Next;
+                }
+                else
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        /// <summary>
+        /// Getting result of intersection of two multiplicity.
+        /// </summary>
+        /// <param name="First"></param>
+        /// <param name="Second"></param>
+        public void Intersection(multiplicity<T> FirstMultiplicity, multiplicity<T> SecondMultiplicity)
+        {
+            this.head = null;
+
+            if ((FirstMultiplicity.IsEmpty()) || (SecondMultiplicity.IsEmpty()))
+            {
+                this.head = null;
+                return;
+            }
+
+            MultiplicityElement currentFirst = FirstMultiplicity.head;
+            MultiplicityElement currentSecond = SecondMultiplicity.head;
+
+            while (currentFirst != null)
+            {
+                while (currentSecond != null)
+                {
+                    if (currentFirst.Value.Equals(currentSecond.Value))
+                    {
+                        this.AddElement(currentFirst.Value);
+                    }
+                    currentSecond = currentSecond.Next;
+                }
+                currentFirst = currentFirst.Next;
+                currentSecond = SecondMultiplicity.head;
+            }
+        }
+
+        /// <summary>
+        /// Getting result of union of two multiplicity.
+        /// </summary>
+        /// <param name="First"></param>
+        /// <param name="Second"></param>
+        public void Union(multiplicity<T> FirstMultiplicity, multiplicity<T> SecondMultiplicity)
+        {
+            this.head = null;
+
+            MultiplicityElement currentFirst = FirstMultiplicity.head;
+            MultiplicityElement currentSecond = SecondMultiplicity.head;
+
+            while (currentFirst != null)
+            {
+                this.AddElement(currentFirst.Value);
+                currentFirst = currentFirst.Next;
+            }
+
+            while (currentSecond != null)
+            {
+                this.AddElement(currentSecond.Value);
+                currentSecond = currentSecond.Next;
+            }
+        }
+    }
+}
---
+++ second semestr/HW7/2.7.2/multiplicityTest/multiplicityTest.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using _2._7._2;
+
+namespace multiplicityTest
+{
+    [TestClass]
+    public class multiplicityTest
+    {
+
+        [TestMethod]
+        public void IsEmptyTest()
+        {
+            multiplicity<int> multiplicity = new multiplicity<int>();
+            Assert.IsTrue(multiplicity.IsEmpty());
+        }
+
+        [TestMethod]
+        public void AddIntElement()
+        {
+            multiplicity<int> multiplicity = new multiplicity<int>();
+            multiplicity.AddElement(88);
+            Assert.IsTrue(multiplicity.availabilityOFValue(88));
+        }
+
+        [TestMethod]
+        public void AddStringElementTest()
+        {
+            multiplicity<string> multiplicity = new multiplicity<string>();
+            multiplicity.AddElement("c#");
+            Assert.IsTrue(multiplicity.availabilityOFValue("c#"));
+        }
+
+        [TestMethod]
+        public void DeleteElementTest()
+        {
+            multiplicity<int> multiplicity = new multiplicity<int>();
+            multiplicity.AddElement(8);
+            multiplicity.DeleteElement(8);
+            Assert.IsTrue(multiplicity.IsEmpty());
+        }
+
+        [TestMethod]
+        public void EmptyIntersectionTest()
+        {
+            multiplicity<int> multiplicity1 = new multiplicity<int>();
+            multiplicity1.AddElement(8);
+            multiplicity<int> multiplicity2 = new multiplicity<int>();
+            multiplicity2.AddElement(2);
+            multiplicity<int> intersection = new multiplicity<int>();
+            intersection.AddElement(9876);
+            intersection.Intersection(multiplicity1, multiplicity2);
+            Assert.IsTrue(intersection.IsEmpty());
+        }
+
+        [TestMethod]
+        public void IntersectionTest()
+        {
+            multiplicity<int> multiplicity1 = new multiplicity<int>();
+            multiplicity1.AddElement(8);
+            multiplicity1.AddElement(1);
+            multiplicity<int> multiplicity2 = new multiplicity<int>();
+            multiplicity2.AddElement(3);
+            multiplicity2.AddElement(8);
+            multiplicity<int> intersection = new multiplicity<int>();
+            intersection.Intersection(multiplicity1, multiplicity2);
+            Assert.IsFalse(intersection.IsEmpty());
+        }
+
+        [TestMethod]
+        public void UnionTest()
+        {
+            multiplicity<int> multiplicity1 = new multiplicity<int>();
+            multiplicity1.AddElement(1);
+            multiplicity<int> multiplicity2 = new multiplicity<int>();
+            multiplicity1.AddElement(9);
+            multiplicity<int> union = new multiplicity<int>();
+            union.Union(multiplicity1, multiplicity2);
+            Assert.IsFalse(union.IsEmpty());
+        }
+    }
+}
