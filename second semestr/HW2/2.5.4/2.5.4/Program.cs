f91f72c493e733c730596000ac92e1292ba45beb
Dasha-Belozerceva <d_belosertseva@mail.ru>
2015-05-28 11:32:56 +0300

    cursor control

---
+++ second semestr/HW2/2.5.4/2.5.4/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2._5._4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var eventLoop = new EventLoop();
+            var cursorMove = new CursorMove();
+
+            eventLoop.LeftHandler += cursorMove.OnLeft;
+            eventLoop.RightHandler += cursorMove.OnRight;
+            eventLoop.UpHandler += cursorMove.OnUp;
+            eventLoop.DownHandler += cursorMove.OnDown;
+
+            eventLoop.Run();
+        }
+    }
+}
---
+++ second semestr/HW2/2.5.4/2.5.4/list.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2._5._4
+{
+    /// <summary>
+    /// List
+    /// </summary>
+    public class List<T> : IEnumerable<T>
+    {
+        public class ListElement
+        {
+            public T Value { get; set; }
+            public ListElement (T value)
+            {
+                Value = value;
+            }
+            public ListElement Next { get; set; }
+        }
+
+        private ListElement head = null;
+
+        /// <summary>
+        /// checking list for empty
+        /// </summary>
+        /// <returns>"true" if list is empty, else false</returns>
+        public bool  IsEmpty()
+        {
+            return head == null;
+        }
+
+        public void InsertElementToHead(T value)
+        {
+            ListElement newElement = new ListElement(value);
+            {
+                ListElement Next = head;
+                T Value = value;
+            };
+        }
+
+
+    }
+}
