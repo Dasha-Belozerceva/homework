f74450c73f4a3ff598eee9e00e36d300f8a5430e
Dasha-Belozerceva <d_belosertseva@mail.ru>
2015-05-28 12:17:25 +0300

    2.3.2

---
+++ second semestr/HW3/2.3.2/HashTable.Tests/hashTableTest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace HashTable.Tests
+{
+    [TestClass]
+    public class hashTableTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
