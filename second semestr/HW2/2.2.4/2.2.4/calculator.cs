3f996ed55ba140bff464b919e92b73d6bf6728be
Dasha-Belozerceva <d_belosertseva@mail.ru>
2015-05-28 11:30:42 +0300

    stack calculator

---
+++ second semestr/HW2/2.2.4/2.2.4/calculator.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using StackInArrayNamespace;
+using StackInListNamespace;
+
+namespace _2._2._4
+{
+    public class Calculator
+    {
+        private InterfaceStack Stack;
+
+        public Calculator(InterfaceStack stack)
+        {
+            this.Stack = stack;
+        }
+        
+
+        /// <summary>
+        /// Calculate sum.
+        /// </summary>
+        /// <returns></returns>
+        public int Sum()
+        {
+            int firstNumber = Stack.ReturnPopElement();
+            int secondNumber = Stack.ReturnPopElement();
+            int sum = firstNumber + secondNumber;
+            return sum;
+        }
+
+        /// <summary>
+        /// Calculate difference.
+        /// </summary>
+        /// <returns></returns>
+        public int Difference()
+        {
+            int firstNumber = Stack.ReturnPopElement();
+            int secondNumber = Stack.ReturnPopElement();
+            int difference = firstNumber - secondNumber;
+            return difference;
+        }
+
+        /// <summary>
+        /// Difference quotient.
+        /// </summary>
+        /// <returns></returns>
+        public int Quotient()
+        {
+            int firstNumber = Stack.ReturnPopElement();
+            int secondNumber = Stack.ReturnPopElement();
+            int quotient = firstNumber / secondNumber;
+            return quotient;
+        }
+
+        /// <summary>
+        /// Quotient product.
+        /// </summary>
+        /// <returns></returns>
+        public int Product()
+        {
+            int firstNumber = Stack.ReturnPopElement();
+            int secondNumber = Stack.ReturnPopElement();
+            int product = firstNumber * secondNumber;
+            return product;
+        }
+    }
+}
